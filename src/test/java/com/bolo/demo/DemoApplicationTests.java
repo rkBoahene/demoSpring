49c571cf9335dd0618b5367a02955973998d9ecb
Richmond Boahene <rkboahene2@gmail.com>
2022-09-14 11:40:35 +0000

    initialize new spring demo app

---
+++ src/test/java/com/bolo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
